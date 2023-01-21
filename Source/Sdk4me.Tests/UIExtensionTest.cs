90abfa8c19dd731887a78f5c28dc905761f7b891
Klaas Vandeweerdt <klaas_vandeweerdt@hotmail.com>
2023-01-22 00:30:16 +0100

    Release 2.0.8

---
+++ Source/Sdk4me.Tests/UIExtensionTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Sdk4me.Tests
+{
+    [TestClass]
+    public class UIExtensionTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            Sdk4meClient client = Client.Get();
+            List<UIExtension> extensions = client.UIExtensions.Get("*");
+            Assert.IsNotNull(extensions);
+            Assert.IsInstanceOfType(extensions, typeof(List<UIExtension>));
+        }
+    }
+}
