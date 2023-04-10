71f23f3d444fbaf45ea90f8041aef6d4577e0bdb
Klaas Vandeweerdt <klaas_vandeweerdt@hotmail.com>
2023-04-10 14:35:12 +0200

    Updated KnowledgeArticleTemplateTest

--- Source/Sdk4me.Tests/KnowledgeArticleTemplateTest.cs
+++ Source/Sdk4me.Tests/KnowledgeArticleTemplateTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Sdk4me.Tests
 {
@@ -15,6 +16,23 @@ namespace Sdk4me.Tests
             Console.WriteLine($"Count: {knowledgeArticleTemplates.Count}");
             Assert.IsNotNull(knowledgeArticleTemplates);
             Assert.IsInstanceOfType(knowledgeArticleTemplates, typeof(List<KnowledgeArticleTemplate>));
+
+            if (knowledgeArticleTemplates.Count == 0)
+                return;
+            KnowledgeArticleTemplate knowledgeArticleTemplate = knowledgeArticleTemplates[Random.Shared.Next(knowledgeArticleTemplates.Count)];
+            Trace.WriteLine($"Continue tests on knowledge article #{knowledgeArticleTemplate.ID}, {knowledgeArticleTemplate.Subject}");
+
+            Trace.WriteLine($"Get service instance for knowledge article template #{knowledgeArticleTemplate.ID}");
+            List<ServiceInstance> serviceInstances = client.KnowledgeArticleTemplates.GetServiceInstances(knowledgeArticleTemplate, "*");
+            Console.WriteLine($"Count: {serviceInstances.Count}");
+            Assert.IsNotNull(serviceInstances);
+            Assert.IsInstanceOfType(serviceInstances, typeof(List<ServiceInstance>));
+
+            Trace.WriteLine($"Get knowledge article for knowledge article template #{knowledgeArticleTemplate.ID}");
+            List<KnowledgeArticle> knowledgeArticles = client.KnowledgeArticleTemplates.GetKnowledgeArticles(knowledgeArticleTemplate);
+            Console.WriteLine($"Count: {knowledgeArticles.Count}");
+            Assert.IsNotNull(knowledgeArticles);
+            Assert.IsInstanceOfType(knowledgeArticles, typeof(List<KnowledgeArticle>));
         }
     }
 }
